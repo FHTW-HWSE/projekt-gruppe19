5ee44c87f972e5f1509604e38913936b41cc15ff
ic22b039 <ic22b039@technikum-wien.at>
2023-04-15 21:00:24 +0200

    logfile_function

---
+++ logfile_function.c
@@ -0,0 +1,13 @@
+void logFile(const char *log_pfad, char data){
+
+    FILE *file = fopen(log_pfad, "a");
+	if (file == NULL) {
+		fprintf(stderr, "Fehler, die Datei konnte nicht geöffnet werden %s\n",
+				log_pfad);
+		return;
+	}
+
+	fprintf(file, "%s\n", data);
+
+	fclose(file);
+}
