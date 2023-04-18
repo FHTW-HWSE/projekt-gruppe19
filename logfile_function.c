3652284d3780cf75f2f1200d96152e48e4e46e34
Markus <129212790+ic22b039@users.noreply.github.com>
2023-04-18 18:02:03 +0200

    Update logfile_function.c

--- logfile_function.c
+++ logfile_function.c
@@ -10,3 +10,24 @@ void logFile(const char *log_pfad, char data){
 
 	fclose(file);
 }
+
+
+void time_track(const char *time_pfad, char *data) {
+
+	time_t current_time = time(NULL);
+	struct tm *time_info = localtime(&current_time);
+	char time_string[20];
+	strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", time_info);
+
+	FILE *file = fopen(time_pfad, "a");
+	if (file == NULL) {
+		fprintf(stderr, "Fehler, die Datei konnte nicht geöffnet werden %s\n",
+				time_pfad);
+		return;
+	}
+
+	fprintf(file, "Um: %s\nWurde der Buchstabe: %s\t eingegeben\n\n",
+			time_string, data);
+
+	fclose(file);
+}
